7b2bf4f06edc2a148d7e4e1e54040b907d60afa9
Adam Soltys <asoltys@gmail.com>
2009-04-29 00:57:34 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,82 @@
+<html><!-- #BeginTemplate "/Templates/BridgeClub2.dwt" -->
+<head>
+<!-- #BeginEditable "doctitle" --> 
+<title>Untitled Document</title>
+<!-- #EndEditable -->
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body bgcolor="#FFFFFF" text="#000000">
+<table width="98%" border="1" cellspacing="2" cellpadding="2" bordercolor="#999999">
+  <tr align="center" valign="top" bgcolor="#000066"> 
+    <td colspan="3" height="62"><img src="/Images/Sunset.jpg" width="100%" height="73"></td>
+  </tr>
+  <tr align="center" valign="top"> 
+    <td rowspan="2" width="20%" bgcolor="#CCFFCC" background="/Images/Water%20lilies.jpg"> 
+      <table cellspacing="1" cellpadding="1" border="0"
+ width="100%" name="Navigation" align="center">
+        <tbody> 
+        <tr bgcolor="#FFFFCC"> 
+          <td width="14" valign="top"><b><font size="2" color="#333333"><img height="12" alt=""
+ hspace="3" src="file:///C|/Program%20Files/Macromedia/Dreamweaver%204/sites/meditateinvictoria/images/arrow.gif" width="8" align="left" border="0"></font></b></td>
+          <td width="125"><b><a href="/index.html"><font face="Arial, Helvetica, sans-serif" size="2" color="#333333">Home 
+            Page</font></a></b></td>
+        </tr>
+        <tr bgcolor="#FFFFCC"> 
+          <td valign="top"><b><font size="2" color="#333333"><img height="12" alt=""
+ hspace="3" src="file:///C|/Program%20Files/Macromedia/Dreamweaver%204/sites/meditateinvictoria/images/arrow.gif" width="8" align="left" border="0"></font></b></td>
+          <td><b><a href="/weekly.html"><font face="Arial, Helvetica, sans-serif" size="2" color="#333333">Weekly 
+            Schedule </font></a></b></td>
+        </tr>
+        <tr bgcolor="#FFFFCC"> 
+          <td valign="top" height="22"><b><font size="2" color="#333333"><img height="12" alt=""
+ hspace="3" src="file:///C|/Program%20Files/Macromedia/Dreamweaver%204/sites/meditateinvictoria/images/arrow.gif" width="8" align="left" border="0"></font></b></td>
+          <td><b><a href="/calendar.html"><font face="Arial, Helvetica, sans-serif" size="2" color="#333333">Calendar</font></a></b></td>
+        </tr>
+        <tr bgcolor="#FFFFCC"> 
+          <td valign="top" height="20"><b><font size="2" color="#333333"><img height="12"
+ alt="" hspace="3" src="file:///C|/Program%20Files/Macromedia/Dreamweaver%204/sites/meditateinvictoria/images/arrow.gif" width="8" align="left"
+ border="0"></font></b></td>
+          <td height="20"><b><a href="/events.html"><font face="Arial, Helvetica, sans-serif" size="2" color="#333333">Special 
+            Events </font></a></b></td>
+        </tr>
+        <tr bgcolor="#FFFFCC"> 
+          <td valign="top" height="22"><b><font size="2" color="#333333"><img height="12" alt=""
+ hspace="3" src="file:///C|/Program%20Files/Macromedia/Dreamweaver%204/sites/meditateinvictoria/images/arrow.gif" width="8" align="left" border="0"></font></b></td>
+          <td><b><a href="/results.html"><font face="Arial, Helvetica, sans-serif" size="2" color="#333333">Game 
+            Results </font></a></b></td>
+        </tr>
+        <tr bgcolor="#FFFFCC"> 
+          <td valign="top" height="15"><b><font size="2" color="#333333"><img height="12" alt=""
+ hspace="3" src="file:///C|/Program%20Files/Macromedia/Dreamweaver%204/sites/meditateinvictoria/images/arrow.gif" width="8" align="left" border="0"></font></b></td>
+          <td><b><a href="/links.html"><font face="Arial, Helvetica, sans-serif" size="2" color="#333333">Links</font></a></b></td>
+        </tr>
+        <tr bgcolor="#FFFFCC"> 
+          <td valign="top" height="15"><b><font size="2" color="#333333"><img height="12" alt=""
+ hspace="3" src="file:///C|/Program%20Files/Macromedia/Dreamweaver%204/sites/meditateinvictoria/images/arrow.gif" width="8" align="left" border="0"></font></b></td>
+          <td><b><a href="/contact.html"><font face="Arial, Helvetica, sans-serif" size="2" color="#333333">Contact</font></a></b></td>
+        </tr>
+        </tbody> 
+      </table>
+      <!-- #BeginEditable "LeftText" -->{LeftText}<!-- #EndEditable --></td>
+    <td height="450" bgcolor="#CCCCCC"><!-- #BeginEditable "CenterText" -->Home 
+      Page <!-- #EndEditable --></td>
+    <td rowspan="2" width="10%" bgcolor="#CCFFCC" background="/Images/Winter.jpg"><!-- #BeginEditable "RightText" -->{RightText}<!-- #EndEditable --></td>
+  </tr>
+  <tr>
+    <td align="center" valign="bottom" height="50" bgcolor="#99FFFF"> 
+      <div align="center"> </div>
+      <p align="center"><font face="Arial, Helvetica, sans-serif" size="2" color="#663366"><b>Ewa's Bridge Club</b></font><font face="Arial, Helvetica, sans-serif" size="2" color="#6666CC"><br>
+        </font><font color="#6666CC"><font
+ face="Arial, Helvetica, sans-serif">12345 Anywhere Street<font size="1"><br>
+        Victoria, British Columbia&nbsp;&nbsp;V8R 2B2</font>&nbsp; Canada</font><br>
+        <small><font face="Arial, Helvetica, sans-serif">telephone: (250) 555-1234<br>
+        </font></small> <small><font face="Arial, Helvetica, sans-serif"
+ size="2"> email<big>:&nbsp; <a
+ href="mailto:EwasBridge@shaw.ca">EwasBridge@shaw.ca</a>&nbsp;</big></font></small> 
+        </font></p>
+                      </td>
+  </tr>
+</table>
+</body>
+<!-- #EndTemplate --></html>
