4616c14387637294d4cd5d1b64ea81b54cdca39e
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2010-12-07 20:52:26 -0700

    adding christmas event to events page

--- events.php
+++ events.php
@@ -1,21 +1,14 @@
 <html>
-<!-- DW6 -->
 <head>
 
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 <meta name="robots" content="index,follow">
 <meta name="author" content="Brian Latta">
 
-<!--//if (empty ($metaDescription))-->
-
 <meta name="description" content="Capital City Bridge Club">
 <meta name="keywords" content="victoria, bridge, club">
 
 <title> Capital City Bridge Club</title>
- 
-
-<title>Untitled Document</title>
-
 
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 </head>
@@ -29,33 +22,23 @@
     <td rowspan="2" width="20%" bgcolor="#33CC66" bordercolor="#FF0000"> 
       <?php require('navigation.php') ?>
     </td>
-    <td height="500" bgcolor="#33CC66" width="67%" bordercolor="#333333">
-
+    <td height="500" bgcolor="#33CC66" width="67%" bordercolor="#333333" style="font-family: arial; font-size: 18px;">
       <h1>SPECIAL EVENTS</h1>
 				<hr />
 					
-				<p>We invite you to come and play at a fun event:</p>
-
-							
-		    <p>
-          the <span style="font-size: 18px">ERRIN BERRY GAME</span><br />
-				  Wednesday, October 27th, 2010<br />
-          at 6.45 pm
+        <p style="font-size: 40px; color: red">
+				CELEBRATING CHRISTMAS
         </p>
-
-<p>
-<strong>At least one member of the partnership must have less than 50 masterpoints.</strong><br />
-(Let us know in advance if you don't have a partner, and we will find one for you)
-</p>
-
-<p>
-This is a special game, with  the same hands being played across the country.<br />
-Top percentage score in each Zone will receive plaques and all club winners will receive certificates.<br />
-Masterpoint awards are 68% sectionally rated black points.<br />
-Entry fee $8 per person
-</p>
-
-			
+       	
+        <p>
+				<em>Friday, December 17th</em> - <span style="color: #040">CLUB CHAMPIONSHIP</span>  with <span style="color: red">CHRISTMAS GOODIES</span>
+        </p>
+	    
+        <p>
+          <em>Wednesday, December 22nd &amp; Thursday, December 23rd</em><br />
+          <span style="color: #040">CLUB CHAMPIONSHIPS &amp; CHRISTMAS CELEBRATIONS</span><br />
+          including <span style="color: red">CHRISTMAS GOODIES AND PRIZES</span>
+        </p>
 			</td>
   </tr>
   <tr>
