76dc2c3121790a18555583e438439b1959416bd6
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2013-01-25 21:19:43 -0800

    February calendar

---
+++ feb2013.php
@@ -0,0 +1,331 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+
+  <meta http-equiv="Content-Type"
+ content="text/html; charset=iso-8859-1">
+  <meta name="robots" content="index,follow">
+  <meta name="author" content="Brian Latta">
+<!--//if (empty ($metaDescription))-->
+  <meta name="description" content="Capital City Bridge Club">
+  <meta name="keywords" content="victoria, bridge, club">
+  <title>Capital City Bridge Club</title>
+
+  <meta http-equiv="Content-Type"
+ content="text/html; charset=iso-8859-1">
+</head>
+<body style="color: rgb(0, 0, 0); background-color: rgb(255, 255, 255);">
+<table border="1" bordercolor="#999999" cellpadding="2" cellspacing="2"
+ width="98%">
+  <tbody>
+    <tr align="center" bgcolor="#33cc66" valign="top">
+      <td colspan="3" height="97"><img
+ src="/Images/CapitalCityBridgeClub3.jpg" height="90" width="800"></td>
+    </tr>
+    <tr align="center" valign="top">
+      <td rowspan="2" bordercolor="#FF0000" bgcolor="#33cc66"
+ width="20%">
+       <?php include('navigation.php') ?>
+      </td>      <td bordercolor="#333333" bgcolor="#33cc66" height="415"
+
+ width="67%"><!-- #BeginEditable "CenterText" -->
+
+      <table border="1" cellpadding="0" cellspacing="0" width="98%">
+
+        <tbody>
+
+          <tr align="center">
+
+            <td colspan="7" height="50">
+
+            <div align="center">
+              <a href="jan2013.php">Previous</a> - February 2013 - <a href="mar2013.php">Next</a></div>
+
+            </td>
+
+          </tr>
+
+          <tr align="center" valign="top">
+
+            <td height="30" nowrap="nowrap" width="14%"><b>Sun</b></td>
+
+            <td height="30" nowrap="nowrap" width="14%"><b>Mon</b></td>
+
+            <td height="30" nowrap="nowrap" width="14%"><b>Tues</b></td>
+
+            <td height="30" width="14%"><b>Wed</b></td>
+
+            <td height="30" nowrap="nowrap" width="14%"><b>Thur</b></td>
+
+            <td height="30" nowrap="nowrap" width="14%"><b>Fri</b></td>
+
+            <td height="30" nowrap="nowrap"><b>Sat</b></td>
+
+          </tr>
+
+          <tr align="right" valign="top">
+
+            <td height="70" nowrap="nowrap" width="14%">
+
+            <p><br>
+												</p>
+
+            </td>
+
+            <td height="70" nowrap="nowrap" width="14%">
+
+            <p></p>
+
+            <p><br>
+												</p>
+
+            </td>
+
+            <td height="70" nowrap="nowrap" width="14%">
+
+            <p><br>
+												</p>
+
+            </td>
+
+            <td height="70" nowrap="nowrap" width="14%">
+
+            <p><br>
+												</p>
+
+            </td>
+
+            <td height="70" nowrap="nowrap" width="14%">
+											</td>
+
+            <td height="70" nowrap="nowrap" width="14%">1
+            
+              <p><a href="/Results/2008/130201.txt">Results</a></p>
+            </td>
+
+            <td height="70" nowrap="nowrap">2</td>
+
+          </tr>
+
+          <tr align="right" valign="top">
+
+            <td height="69" nowrap="nowrap" width="14%">
+
+            <div align="right">
+													3</div>
+
+            </td>
+
+            <td height="69" nowrap="nowrap" width="14%">
+
+            <p align="right">4</p>
+
+
+
+            </td>
+
+            <td height="69" nowrap="nowrap" width="14%">
+
+            <div align="right">
+
+            <p>5</p>
+
+            </div>
+
+            </td>
+
+            <td height="69" nowrap="nowrap" width="14%">
+
+            <div align="right">
+													6</div>
+              <p><a href="/Results/2008/130206.txt">Results</a></p>
+
+            </td>
+
+            <td height="69" nowrap="nowrap" width="14%">
+
+            <p align="right">7</p>
+              <p><a href="/Results/2008/130207.txt">Results</a></p>
+
+            </td>
+
+            <td height="69" nowrap="nowrap" width="14%">
+
+            <div align="right">
+													8</div>
+              <p><a href="/Results/2008/130208.txt">Results</a></p>
+
+            </td>
+
+            <td height="69" nowrap="nowrap">
+
+            <div align="right">
+													9</div>
+
+            </td>
+
+          </tr>
+
+          <tr align="right" valign="top">
+
+            <td height="70" nowrap="nowrap" width="14%">
+
+            <div align="right">
+													10</div>
+
+            </td>
+
+            <td height="70" nowrap="nowrap" width="14%">
+
+            <div align="right">
+              11
+              
+
+												</div>
+
+            </td>
+
+            <td height="70" nowrap="nowrap" width="14%">
+
+            <p align="right">12</p>
+
+            </td>
+
+            <td height="70" nowrap="nowrap" width="14%">
+              <div align="right">13</div>
+              <p><a href="/Results/2008/130213.txt">Results</a></p>
+            </td>
+
+            <td height="70" nowrap="nowrap" width="14%">
+
+            <p align="right">14</p>
+              <p><a href="/Results/2008/130214.txt">Results</a></p>
+
+            </td>
+
+            <td height="70" nowrap="nowrap" width="14%">
+
+            <div align="right">
+													15</div>
+              <p><a href="/Results/2008/130215.txt">Results</a></p>
+
+            </td>
+
+            <td height="70" nowrap="nowrap">
+
+            <div align="right">
+													16</div>
+
+            </td>
+
+          </tr>
+
+          <tr align="right" valign="top">
+
+            <td height="73" nowrap="nowrap" width="14%">
+
+            <div align="right">
+
+            <p>17</p>
+
+            </div>
+
+            </td>
+
+            <td height="73" nowrap="nowrap" width="14%">
+
+            <div align="right">
+
+            <p>18</p>
+
+
+            </div>
+
+            </td>
+
+            <td height="73" nowrap="nowrap" width="14%">
+
+            <div align="right">
+													19</div>
+
+            </td>
+
+            <td height="73" nowrap="nowrap" width="14%">
+
+            <div align="right">
+													20</div>
+              <p><a href="/Results/2008/130220.txt">Results</a></p>
+
+            </td>
+
+            <td height="73" nowrap="nowrap" width="14%">
+
+            <div align="right">
+													21
+              <p><a href="/Results/2008/130221.txt">Results</a></p>
+
+												</div>
+
+            </td>
+
+            <td height="73" nowrap="nowrap" width="14%">
+
+            <div align="right">
+													22</div>
+              <p><a href="/Results/2008/130222.txt">Results</a></p>
+
+            </td>
+
+            <td height="73" nowrap="nowrap">
+
+            <div align="right">
+													23</div>
+
+            </td>
+
+          </tr>
+
+          <tr align="right" valign="top">
+
+            <td height="70" nowrap="nowrap" width="14%">24<br>
+											</td>
+
+            <td height="70" nowrap="nowrap" width="14%">
+													25
+
+											</td>
+
+            <td height="70" nowrap="nowrap" width="14%">26<br>
+											</td>
+
+            <td height="70" nowrap="nowrap" width="14%">27<br>
+
+              <p><a href="/Results/2008/130227.txt">Results</a></p>
+											</td>
+
+            <td height="70" nowrap="nowrap" width="14%">28<br>
+
+              <p><a href="/Results/2008/130228.txt">Results</a></p>
+											</td>
+
+            <td height="70" nowrap="nowrap" width="14%"></td>
+            <td height="70" nowrap="nowrap"></td>
+
+          </tr>
+        </tbody>
+      </table>
+      </td>
+      <td rowspan="2" bordercolor="#FF0000" bgcolor="#33cc66"
+ width="15%">
+      <p><img src="/Images/bridgehand.gif" height="100" width="125"></p>      <p>Results from this month's games. </p></td>
+    </tr>
+    <tr>
+      <td align="center" valign="bottom" height="50" bgcolor="#33CC66" bordercolor="#FF0000" width="67%"> 
+        <?php require('footer.php') ?>
+      </td>
+    </tr>
+  </tbody>
+</table>
+
+</body>
+</html>
