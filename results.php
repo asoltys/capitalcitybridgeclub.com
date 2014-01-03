f2120032882c89e011e77e1bde0ed6b247a77bb4
Adam Soltys <asoltys@gmail.com>
2014-01-03 02:09:04 -0800

    January Calendar

--- results.php
+++ results.php
@@ -1,22 +1,15 @@
 <html>
-<!-- DW6 -->
 <head>
 
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 <meta name="robots" content="index,follow">
 <meta name="author" content="Brian Latta">
 
-<!--//if (empty ($metaDescription))-->
-
 <meta name="description" content="Capital City Bridge Club">
 <meta name="keywords" content="victoria, bridge, club">
 
 <title> Capital City Bridge Club</title>
  
-
-<title>Untitled Document</title>
-
-
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 </head>
 
@@ -30,21 +23,21 @@
       <?php require('navigation.php') ?>     </td>
     <td height="500" bgcolor="#33CC66" width="67%" bordercolor="#333333"> 
 
-      <h1>2013 Results</h1>
+      <h1>2014 Results</h1>
 
 <ul style="list-style: none">
-  <li><a href="jan2013.php">January</a></li>
-  <li><a href="feb2013.php">February</a></li>
-  <li><a href="mar2013.php">March</a></li>
-  <li><a href="apr2013.php">April</a></li>
-  <li><a href="may2013.php">May</a></li>
-  <li><a href="june2013.php">June</a></li>
-  <li><a href="july2013.php">July</a></li>
-  <li><a href="aug2013.php">August</a></li>
-  <li><a href="sep2013.php">September</a></li>
-  <li><a href="oct2013.php">October</a></li>
-  <li><a href="nov2013.php">November</a></li>
-  <li><a href="dec2013.php">December</a></li>
+  <li><a href="jan2014.php">January</a></li>
+  <li><a href="feb2014.php">February</a></li>
+  <li><a href="mar2014.php">March</a></li>
+  <li><a href="apr2014.php">April</a></li>
+  <li><a href="may2014.php">May</a></li>
+  <li><a href="june2014.php">June</a></li>
+  <li><a href="july2014.php">July</a></li>
+  <li><a href="aug2014.php">August</a></li>
+  <li><a href="sep2014.php">September</a></li>
+  <li><a href="oct2014.php">October</a></li>
+  <li><a href="nov2014.php">November</a></li>
+  <li><a href="dec2014.php">December</a></li>
 </ul>
 </td>
     <td rowspan="2" width="15%" bgcolor="#33CC66" bordercolor="#FF0000"> 
