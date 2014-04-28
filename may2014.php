a7624487d38ab62689e5bb110f108a7507225227
Adam Soltys <asoltys@gmail.com>
2014-04-27 21:01:52 -0700

    Calendar update

---
+++ may2014.php
@@ -0,0 +1,164 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+
+  <meta http-equiv="Content-Type"
+ content="text/html; charset=iso-8859-1">
+  <meta name="robots" content="index,follow">
+  <meta name="author" content="Brian Latta">
+<!--//if (empty ($metaDescription))-->
+  <meta name="description" content="Capital City Bridge Club">
+  <meta name="keywords" content="victoria, bridge, club">
+  <title>Capital City Bridge Club</title>
+
+  <meta http-equiv="Content-Type"
+ content="text/html; charset=iso-8859-1">
+</head>
+<body style="color: rgb(0, 0, 0); background-color: rgb(255, 255, 255);">
+<table border="1" bordercolor="#999999" cellpadding="2" cellspacing="2"
+ width="98%">
+  <tbody>
+    <tr align="center" bgcolor="#33cc66" valign="top">
+      <td colspan="3" height="97"><img
+ src="/images/CapitalCityBridgeClub3.jpg" height="90" width="800"></td>
+    </tr>
+    <tr align="center" valign="top">
+      <td rowspan="2" bordercolor="#FF0000" bgcolor="#33cc66"
+ width="20%">
+       <?php include('navigation.php') ?>
+      </td>
+      <td bordercolor="#333333" bgcolor="#33cc66" height="415"
+ width="67%">        <table border="1" cellpadding="0" cellspacing="0" width="98%">
+          <tbody>
+            <tr align="center">
+              <td colspan="7" height="50">
+                <div align="center"><a href="apr2014.php">Previous</a> - May 2014 - <a href="june2014.php">Next </a></div>
+              </td>
+            </tr>
+            <tr align="center" valign="top"> 
+              <td width="95" height="30"><b>Sun</b></td>
+            <td width="87" height="30"><b>Mon</b></td>
+            <td width="82" height="30"><b>Tues</b></td>
+            <td width="74" height="30"><b>Wed</b></td>
+            <td width="79" height="30"><b>Thur</b></td>
+            <td width="74" height="30"><b>Fri</b></td>
+            <td width="76" height="30"><b>Sat</b></td>
+          </tr>
+            <tr align="right" valign="top"> 
+              <td width="95" height="70"></td>
+            <td width="87" height="70"></td>
+            <td width="82" height="70"></td>
+            <td width="74" height="70"></td>
+            <td width="79" height="70">
+              <p>1</p>
+	             <p><a href="Results/2008/140501.txt">Results</a></p>            </td>
+            <td width="79" height="70">
+              <p>2</p>
+	             <p><a href="Results/2008/140502.txt">Results</a></p>            </td>
+            <td width="76" height="70">3</td>
+          </tr>
+            <tr align="right" valign="top"> 
+              <td width="95" height="69"> 
+                <div align="right">4</div>            </td>
+            <td width="87" height="69"> 
+              <p align="right">5</p>
+                         </td>
+            <td width="82" height="69"> 
+              <p>6</p>            </td>
+            <td width="74" height="69"> 
+              <div align="right">7</div>
+							<p align="right"><a href="Results/2008/140507.txt">Results</a></p>             </td>
+            <td width="79" height="69"> 
+              <p align="right">8</p>
+              <p align="right"><a href="Results/2008/140508.txt">Results</a></p>            </td>
+            <td width="74" height="69"> 
+              <div align="right">9</div>            
+							<p align="right"><a href="Results/2008/140509.txt">Results</a></p> 
+							</td>
+            <td width="76" height="69"> 
+              <div align="right">10</div>            </td>
+          </tr>
+            <tr align="right" valign="top"> 
+              <td width="95" height="70"> 
+                <div align="right">11</div>            </td>
+            <td width="87" height="70"> 
+              <p>12</p>
+                       </td>
+            <td width="82" height="70"> 
+              <p align="right">13</p>
+           </td>
+            <td width="74" height="70"> 
+              <div align="right">14</div>
+							<p><a href="Results/2008/140514.txt">Results</a></p>     
+							            </td>
+            <td width="79" height="70"> 
+              <p align="right">15</p>
+              <p align="right"><a href="Results/2008/140515.txt">Results</a></p>            </td>
+            <td width="74" height="70"> 
+              16     
+						<p><a href="Results/2008/140516.txt">Results</a></p>  	
+							         </td>
+            <td width="76" height="70"> 
+              <div align="right">17</div>            </td>
+          </tr>
+            <tr align="right" valign="top"> 
+              <td width="95" height="73"> 
+                <div align="right"> 
+                  <p>18</p>
+                <p></p>
+              </div>            </td>
+            <td width="87" height="73"> 
+              <p>19</p>
+            </td>
+            <td width="82" height="73"> 
+              <div align="right">20</div>            </td>
+            <td width="74" height="73"> 
+              <div align="right">21</div>
+							                <p><a href="Results/2008/140521.txt">Results</a></p>
+							            </td>
+            <td width="79" height="73"> 
+              <p>22</p>
+                <p><a href="Results/2008/140522.txt">Results</a></p>            </td>
+            <td width="74" height="73"> 
+              <div align="right">23</div>    
+							                <p><a href="Results/2008/140523.txt">Results</a></p>
+							        </td>
+            <td width="76" height="73"> 
+              <div align="right">24</div>            </td>
+          </tr>
+            <tr align="right" valign="top"> 
+              <td width="95" height="70">
+                <p>25</p>
+              <p>&nbsp;</p>            </td>
+            <td width="87" height="70">
+              <p>26</p>
+                        </td>
+            <td width="82" height="70">27</td>
+            <td width="74" height="70">28
+						<p><a href="Results/2008/140528.txt">Results</a></p>  
+						</td>
+            <td width="79" height="70">
+              <p>29</p>  
+		<p><a href="Results/2008/140529.txt">Results</a></p>  
+          </td>
+            <td width="74" height="70">30
+		<p><a href="Results/2008/140530.txt">Results</a></p>  
+</td>
+            <td width="76" height="70">31</td>
+          </tr>
+        </table></td>
+      <td rowspan="2" bordercolor="#FF0000" bgcolor="#33cc66"
+ width="15%">
+      <p><img src="/images/bridgehand.gif" height="100" width="125"></p>      <p>Results from this month's games. </p></td>
+    </tr>
+    <tr>
+      <td bordercolor="#FF0000" align="center" bgcolor="#33cc66"
+ height="50" valign="bottom" width="67%">
+        <?php include('footer.php') ?>
+      </td>
+    </tr>
+  </tbody>
+</table>
+
+</body>
+</html>
